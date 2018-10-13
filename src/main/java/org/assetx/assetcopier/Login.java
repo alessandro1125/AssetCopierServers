1bd7f400c618b61f205a717da41f65ec4e810f03
Alessandro Giordano <alessandro.giordano2000@gmail.com>
2018-10-13 21:39:10 +0200

    tst

--- src/main/java/org/assetx/assetcopier/Login.java
+++ src/main/java/org/assetx/assetcopier/Login.java
@@ -48,6 +48,7 @@ public class Login extends HttpServlet {
 
         // path protocol
         String patList[] = req.getPathInfo().split("/");
+        System.out.println(req.getPathInfo());
         for(String path : patList)
             System.out.println(path+ "--exx");
         if (patList.length > 0) {
